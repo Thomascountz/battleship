f4693f5946f5c8145e50ee5f8e3fb7bac7959eee
Thomas Countz <thomascountz@gmail.com>
2018-05-09 10:42:59 -0400

    add #start to game engine class

---
+++ lib/engine.rb
@@ -0,0 +1,15 @@
+require_relative 'console_ui'
+
+class Engine
+  def initialize(interface: ConsoleUI.new)
+    @interface = interface
+  end
+
+  def start
+    interface.greet
+  end
+
+  private
+
+  attr_reader :interface
+end
