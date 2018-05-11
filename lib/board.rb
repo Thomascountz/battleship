a8a17311fa6f5f5de8de36f369371291a625e5b6
Thomas Countz <thomascountz@gmail.com>
2018-05-11 11:36:54 -0400

    add board play_area

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,14 @@
 class Board
+  attr_reader :play_area
+
+  def initialize
+    @play_area = []
+    10.times do
+      row = []
+      10.times do
+        row << nil
+      end
+      @play_area << row
+    end
+  end
 end
---
+++ spec/board_spec.rb
@@ -0,0 +1,16 @@
+require 'board'
+
+RSpec.describe Board do
+	subject(:board) { described_class.new }
+
+	describe '#play_area' do
+		context 'when initialized' do
+			it 'is a 10x10 Array of Arrays' do
+				expect(board.play_area).to be_a Array
+				expect(board.play_area.first).to be_a Array
+				expect(board.play_area.first.length).to be 10
+				expect(board.play_area.flatten.length).to be 100
+			end
+		end
+	end
+end
