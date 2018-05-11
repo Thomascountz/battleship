9245f78dad76116a196c2e3749b6568a6d1b6401
Thomas Countz <thomascountz@gmail.com>
2018-05-11 13:38:27 -0400

    fix indentation

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,18 +1,18 @@
 require 'board'
 
 RSpec.describe Board do
-	subject(:board) { described_class.new }
+  subject(:board) { described_class.new }
 
-	describe '#play_area' do
-		context 'when initialized' do
-			it 'is a 10x10 Array of Arrays' do
-				expect(board.play_area).to be_a Array
-				expect(board.play_area.first).to be_a Array
-				expect(board.play_area.first.length).to be 10
-				expect(board.play_area.flatten.length).to be 100
-			end
-		end
-	end
+  describe '#play_area' do
+    context 'when initialized' do
+      it 'is a 10x10 Array of Arrays' do
+        expect(board.play_area).to be_a Array
+        expect(board.play_area.first).to be_a Array
+        expect(board.play_area.first.length).to be 10
+        expect(board.play_area.flatten.length).to be 100
+      end
+    end
+  end
 
   describe '#strike_position' do
     context 'when the row and column are valid positions in the play area' do
